743bba8f9e8319b33b12301ae9735381e68b198b
Marco Bernal Jr <bernalmjr@Marcos-MacBook-Air.local>
2014-10-31 08:13:01 -0500

    Testing

--- js/app.js
+++ js/app.js
@@ -12,8 +12,49 @@
         ).then(function(){
             _.templateSettings.interpolate = /{([\s\S]+?)}/g;
 
-            // start app?
-        })
+         var camera, scene, renderer;
+var geometry, material, mesh;
+
+init();
+animate();
+
+function init() {
+
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+    camera.position.z = 1000;
+
+    scene = new THREE.Scene();
+
+    geometry = new THREE.BoxGeometry(250, 200, 250);
+    material = new THREE.MeshBasicMaterial({
+        color: 0xff0000,
+        wireframe: true
+    });
+
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    document.body.appendChild(renderer.domElement);
+
+}
+
+
+
+function animate() {
+
+    requestAnimationFrame(animate);
+
+    mesh.rotation.x += 0.02;
+    mesh.rotation.y += 0.0;
+   mesh.rotation.z += 0.02;
+
+    renderer.render(scene, camera);
+
+}
+        });
 
     }
     
